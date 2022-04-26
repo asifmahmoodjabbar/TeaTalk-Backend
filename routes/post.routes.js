5fbed282c1b363cda35fd6bfd03ecd13e607f4fc
asifmahmoodjabbar <asifmahmoodjabbar@gmail.com>
2022-04-26 17:14:19 +0200

    modifications made in post route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const Post = require("../models/Post.model");
 const User = require("../models/User.model");
 
@@ -6,74 +7,70 @@ const router = express.Router();
 
 //create a post
 router.post("/create", async (req, res) => {
-    const post = await Post.create({
-      user: req.jwtPayload.user._id,
-      description: req.body.description
-    })
-    res.status(200).json(post)
+  const { description } = req.body;
+  const post = await Post.create({
+    description,
+    user: req.jwtPayload.user._id,
+  });
+  res.status(200).json(post);
 });
 
 //update a post
 router.put("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
-    } else {
-      res.status(403).json("you can update only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
+  const { id } = req.params;
+  const { description } = req.body;
+  let post = await Post.findById(id);
+  if (post.user.toString() === req.jwtPayload.user._id) {
+    post.description = description;
+    post = await post.save();
+    res.status(200).json(post);
+  } else {
+    res.status(400).json("unauthorized");
   }
 });
 
 //delete a post
 router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.deleteOne();
-      res.status(200).json("the post has been deleted");
-    } else {
-      res.status(403).json("you can delete only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  if (post.user.toString() === req.jwtPayload.user._id) {
+    await Post.findByIdAndDelete(id);
+    res.status(200).json(post);
+  } else {
+    res.status(400).json("unauthorized");
   }
 });
 
-//like / dislike a post
+/*//like / dislike a post
 router.put("/:id/like", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post.likes.includes(req.body.userId)) {
-      await post.updateOne({ $push: { likes: req.body.userId } });
+  const { id } = req.params;
+    const post = await Post.findById(id);
+    if (!post.likes.includes(req.jwtPayload.user._id)) {
+      await post.updateOne({ $push: { likes: req.jwtPayload.user._id } });
       res.status(200).json("The post has been liked");
     } else {
-      await post.updateOne({ $pull: { likes: req.body.userId } });
+      await post.updateOne({ $pull: { likes: req.jwtPayload.user._id} });
       res.status(200).json("The post has been disliked");
-    }
+    }  
+});
+*/
+
+//get all owned posts
+router.get("/owned", async (req, res) => {
+  try {
+    // find post associated with a user
+    const posts = await Post.find({ user: req.jwtPayload.user._id }).populate("user");
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//get owned posts
-router.get("/owned", async (req, res) => {
-  try {
-        const posts = await Post.find({user: req.jwtPayload.user._id}); 
-        res.status(200).json(posts)
-
-  } catch (err) {
-        res.status(500).json(err);
-  }
-});
-
-//get a post
+//get a post by ID
 router.get("/:id", async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
+    const { id } = req.params;
+    const post = await Post.findById(id);
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
@@ -82,10 +79,9 @@ router.get("/:id", async (req, res) => {
 
 // get all posts
 
-router.get("/", async(req, res) => {
-const posts = await Post.find();
-res.status(200).json(posts);
-})
-
+router.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.status(200).json(posts);
+});
 
 module.exports = router;
